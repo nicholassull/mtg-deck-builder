3496f69925c1f49bbfe27a9024eb2fee9c3cedc1
Nathan Conn <nconn34@gmail.com>
2022-04-24 16:20:53 -0700

    Started to add search functionality
    Co-authored-by: Nathan Conn <nconn34@gmail.com>
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>
    Co-authored-by: Amber Wilwand <amoothenielson@gmail.com>
    Co-authored-by: Chris DePastene <cdepastene@gmail.com>
    Co-authored-by: Erin McCulley <ejmcculley@gmail.com>

--- MTGDeck/Models/Card.cs
+++ MTGDeck/Models/Card.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace MTGDeck.Models
 {
     public class Card
@@ -12,5 +17,14 @@ namespace MTGDeck.Models
         public string Set_Name { get; set; }
         public string Rulings_Uri { get; set; }
         public string Image_Uris { get; set; } 
-    }
+//     public static string SearchCards(string name, string color, string type)
+//     {
+//       var apiCallTask = ApiHelper.Search(name, color, type);//here I use the method I just created
+//       var result = apiCallTask.Result;
+//   //the response from the api is being deserialized just as with the other methods like GetAll() and GetDetails()
+//       JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+//       List<Card> card = JsonConvert.DeserializeObject<List<Card>>(jsonResponse.ToString());
+//       return card;
+//     }
+  }
 }

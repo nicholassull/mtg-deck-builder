9a5bc032c590e6fed0037527680ee16f48048f29
Nathan Conn <nconn34@gmail.com>
2022-04-24 12:15:37 -0700

    Co-authored-by: Nathan Conn <nconn34@gmail.com> Co-authored-by: Nick Sullivan <nicholassull@gmail.com> Co-authored-by: Amber Wilwand <amoothenielson@gmail.com> Co-authored-by: Chris DePastene <cdepastene@gmail.com> Co-authored-by: Erin McCulley <ejmcculley@gmail.com>

--- MTGDeck/ViewModels/RegisterViewModel.cs
+++ MTGDeck/ViewModels/RegisterViewModel.cs
@@ -8,12 +8,10 @@ namespace MTGDeck.ViewModels
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
-
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
-
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]

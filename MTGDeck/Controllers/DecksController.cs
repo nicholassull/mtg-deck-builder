4d6da6273479a197e70304b227c471320c250426
Nathan Conn <nconn34@gmail.com>
2022-05-01 15:45:30 -0700

    Modified Details and Create methods
    Co-authored-by: Nathan Conn <nconn34@gmail.com>
    Co-authored-by: Nick Sullivan <nicholassull@gmail.com>
    Co-authored-by: Amber Wilwand <amoothenielson@gmail.com>
    Co-authored-by: Chris DePastene <cdepastene@gmail.com>
    Co-authored-by: Erin McCulley <ejmcculley@gmail.com>

--- MTGDeck/Controllers/DecksController.cs
+++ MTGDeck/Controllers/DecksController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
+using System;
 
 namespace MTGDeck.Controllers
 {
@@ -29,17 +30,21 @@ namespace MTGDeck.Controllers
       var userDecks = _db.Decks.Where(entry => entry.User.Id == currentUser.Id).ToList();
       return View(userDecks);
     }
+    public ActionResult Create()
+    {
+      return View();
+    }
     [HttpPost]
-    public async Task<ActionResult> Create(Deck deck, int CardId)
+    public async Task<ActionResult> Create(Deck deck, int cardId)
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
       deck.User = currentUser;
       _db.Decks.Add(deck);
       _db.SaveChanges();
-      if (CardId != 0)
+      if (cardId != 0)
       {
-          _db.CardDecks.Add(new CardDeck() { CardId = CardId, DeckId = deck.DeckId });
+          _db.CardDecks.Add(new CardDeck() { CardId = cardId, DeckId = deck.DeckId });
       }
       _db.SaveChanges();
       return RedirectToAction("Index");
@@ -47,11 +52,11 @@ namespace MTGDeck.Controllers
 
     public ActionResult Details(int id)
     {
-      var thisCard = _db.Cards
-          .Include(card => card.JoinEntities)
+      var thisDeck = _db.Decks
+          .Include(deck => deck.JoinEntities)
           .ThenInclude(join => join.Deck)
-          .FirstOrDefault(card => card.CardId == id);
-      return View(thisCard);
+          .FirstOrDefault(deck => deck.DeckId == id);
+      return View(thisDeck);
     } 
 
     public ActionResult Edit(int id)
@@ -80,41 +85,18 @@ namespace MTGDeck.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-        // public ActionResult AddCard(int id)
-    // {
-    //   var thisCard = _db.Cards.FirstOrDefault(card => card.CardId == id);
-    //   ViewBag.CardId = new SelectList(_db.Cards, "CardId", "Name");
-    //   return View(thisCard);
-    // }
-
-    // [HttpPost]int DeckId, string name, string mana_cost, string type_line, string set_name
-    public ActionResult AddCard(Card card, int DeckId)
+    public ActionResult AddCard(Card card, Deck deck, int DeckId, int CardId)
     {
+      Console.WriteLine("=========" + card.Name + "==========");
+      Console.WriteLine("=========" + DeckId + "=========");
       _db.Cards.Add(card);
+      _db.SaveChanges();
       if (DeckId != 0)
       {
-      _db.CardDecks.Add(new CardDeck() { DeckId = DeckId, name = name. });
+      _db.CardDecks.Add(new CardDeck() { CardId = card.CardId, DeckId = deck.DeckId });
       }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-
-    //     public ActionResult AddDeck(int id)
-    // {
-    //   var thisCard = _db.Cards.FirstOrDefault(treat => treat.CardId == id);
-    //   ViewBag.DeckId = new SelectList(_db.Decks, "DeckId", "Name");
-    //   return View(thisCard);
-    // }
-
-    // [HttpPost]
-    // public ActionResult AddDeck(Card card, int DeckId)
-    // {
-    //   if (DeckId != 0)
-    //   {
-    //   _db.CardDecks.Add(new CardDeck() { DeckId = DeckId, CardId = card.CardId });
-    //   }
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
   }
 }
